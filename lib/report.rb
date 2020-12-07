125572d9b0fc26f93bb7bfd823b5ee3d9d73d0ef
Michael Linford <linford.mike@gmail.com>
2020-12-07 11:26:11 +0000

    added test for input of red, and passed

--- lib/report.rb
+++ lib/report.rb
@@ -5,5 +5,7 @@ def report(grades)
 
   output << "\nAmber: 1" if grade_arr.include?("Amber")
 
+  output << "\nRed: 1" if grade_arr.include?("Red")
+
   output
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -7,4 +7,8 @@ describe '#report' do
   it 'input of "Green, Amber" gives output of "Green: 1\nAmber: 1"' do
     expect(report("Green, Amber")).to eq "Green: 1\nAmber: 1"
   end
+
+  it 'input of "Green, Amber" gives an output of "Green: 1\nAmber: 1\nRed: 1"' do
+    expect(report("Green, Amber, Red")).to eq "Green: 1\nAmber: 1\nRed: 1"
+  end
 end
